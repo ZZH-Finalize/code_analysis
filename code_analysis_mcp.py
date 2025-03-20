56531c4193c662d77991e3daced2aa81f122f144
ZZH <1462207500@qq.com>
2025-03-20 17:38:02 +0800

    init commit
    Signed-off-by: ZZH <1462207500@qq.com>

---
+++ code_analysis_mcp.py
@@ -0,0 +1,11 @@
+from pylsp import LspClient
+from mcp.server import Server
+from mcp.server.session import ServerSession
+from mcp.server.stdio import stdio_server
+from mcp.types import (
+    ClientCapabilities,
+    TextContent,
+    Tool,
+    ListRootsResult,
+    RootsCapability,
+)
